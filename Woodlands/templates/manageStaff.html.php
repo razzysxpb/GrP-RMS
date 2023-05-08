2602fa98e8b388e5c878ba2eb768ac42b53f3e13
Raz <43684793+razzysxpb@users.noreply.github.com>
2023-05-08 17:59:44 +0100

    Fix missing GET values

--- Woodlands/templates/manageStaff.html.php
+++ Woodlands/templates/manageStaff.html.php
@@ -1,16 +1,17 @@
 <form method="POST" action="">
-    <input type="text" name="staff[staff_id]" placeholder="Staff ID number">
-    <input type="text" name="staff[status]" placeholder="Staff status">
-    <input type="text" name="staff[dormancy_reason]" placeholder="Reason for dormancy (if applicable)">
-    <input type="text" name="staff[firstname]" placeholder="First name">
-    <input type="text" name="staff[surname]" placeholder="Last name/surname">
-    <input type="text" name="staff[street]" placeholder="Street address">
-    <input type="text" name="staff[town]" placeholder="Town/city">
-    <input type="text" name="staff[postcode]" placeholder="Postcode">
-    <input type="text" name="staff[phone]" placeholder="Phone number">
-    <input type="text" name="staff[email]" placeholder="Email address">
-    <input type="text" name="staff[roles]" placeholder="Staff roles">
-    <input type="text" name="staff[mod_leader]" placeholder="Module leader">
-    <input type="text" name="staff[specialist_subject]" placeholder="Specialist subject (if any)">
+    <input type="text" name="staff[staff_id]" value="<?=$_GET['id']??''?>" placeholder="Staff ID">
+    <input type="text" name="staff[status]" value="<?=$_GET['status']??''?>" placeholder="Status">
+    <input type="text" name="staff[dormancy_reason]" value="<?=$_GET['dormancy_reason']??''?>" placeholder="Dormancy reason">
+    <input type="text" name="staff[firstname]" value="<?=$_GET['firstname']??''?>" placeholder="First name">
+    <input type="text" name="staff[middlename]" value="<?=$_GET['middlename']??''?>" placeholder="Middle name">
+    <input type="text" name="staff[surname]" value="<?=$_GET['surname']??''?>" placeholder="Surname">
+    <input type="text" name="staff[street]" value="<?=$_GET['term_street']??''?>" placeholder="Street">
+    <input type="text" name="staff[town]" value="<?=$_GET['town']??''?>" placeholder="Town">
+    <input type="text" name="staff[postcode]" value="<?=$_GET['postcode']??''?>" placeholder="Postcode">
+    <input type="text" name="staff[phone]" value="<?=$_GET['phone']??''?>" placeholder="Phone number">
+    <input type="text" name="staff[email]" value="<?=$_GET['email']??''?>" placeholder="Email address">
+    <input type="text" name="staff[roles]" value="<?=$_GET['course_id']??''?>" placeholder="Course ID">
+    <input type="text" name="staff[mod_leader]" value="<?=$_GET['home_address']??''?>" placeholder="Home address">
+    <input type="text" name="staff[specialist_subject]" value="<?=$_GET['home_town']??''?>" placeholder="Home town">
     <input type="submit" value="Submit">
 </form>
--- Woodlands/templates/manageStudent.html.php
+++ Woodlands/templates/manageStudent.html.php
@@ -1,20 +1,20 @@
-<form method="POST" action ="">
-    <input type="text" name="student[students_id]" placeholder="Student ID number">
-    <input type="text" name="student[firstname]" placeholder="First name">
-    <input type="text" name="student[middlename]" placeholder="Middle name (if any)">
-    <input type="text" name="student[surname]" placeholder="Last name/surname">
-    <input type="date" name="student[dob]" placeholder="Date of birth">
-    <input type="text" name="student[phone]" placeholder="Phone number">
-    <input type="text" name="student[status]" placeholder="Student status (e.g. active, inactive)">
-    <input type="text" name="student[dormancy_reason]" placeholder="Reason for dormancy (if applicable)">
-    <input type="text" name="student[email]" placeholder="Email address">
-    <input type="text" name="student[course_id]" placeholder="Course ID number">
-    <input type="text" name="student[term_street]" placeholder="Term-time street address">
-    <input type="text" name="student[term_town]" placeholder="Term-time town/city">
-    <input type="text" name="student[term_postcode]" placeholder="Term-time postcode">
-    <input type="text" name="student[home_address]" placeholder="Home address">
-    <input type="text" name="student[home_town]" placeholder="Home town/city">
-    <input type="text" name="student[home_postcode]" placeholder="Home postcode">
-    <input type="text" name="student[password]" placeholder="Password">
-    <input type="submit" value="Submit" />
+<form method="POST" action="">
+    <input type="text" name="student[students_id]" value="<?=$_GET['id']??''?>" placeholder="Student ID">
+    <input type="text" name="student[firstname]" value="<?=$_GET['firstname']??''?>" placeholder="First name">
+    <input type="text" name="student[middlename]" value="<?=$_GET['middlename']??''?>" placeholder="Middle name">
+    <input type="text" name="student[surname]" value="<?=$_GET['surname']??''?>" placeholder="Surname">
+    <input type="date" name="student[dob]" value="<?=$_GET['dob']??''?>" placeholder="Date of birth">
+    <input type="text" name="student[phone]" value="<?=$_GET['phone']??''?>" placeholder="Phone number">
+    <input type="text" name="student[status]" value="<?=$_GET['status']??''?>" placeholder="Status">
+    <input type="text" name="student[dormancy_reason]" value="<?=$_GET['dormancy_reason']??''?>" placeholder="Dormancy reason">
+    <input type="text" name="student[email]" value="<?=$_GET['email']??''?>" placeholder="Email address">
+    <input type="text" name="student[course_id]" value="<?=$_GET['course_id']??''?>" placeholder="Course ID">
+    <input type="text" name="student[term_street]" value="<?=$_GET['term_street']??''?>" placeholder="Term-time street">
+    <input type="text" name="student[term_town]" value="<?=$_GET['term_town']??''?>" placeholder="Term-time town">
+    <input type="text" name="student[term_postcode]" value="<?=$_GET['term_postcode']??''?>" placeholder="Term-time postcode">
+    <input type="text" name="student[home_address]" value="<?=$_GET['home_address']??''?>" placeholder="Home address">
+    <input type="text" name="student[home_town]" value="<?=$_GET['home_town']??''?>" placeholder="Home town">
+    <input type="text" name="student[home_postcode]" value="<?=$_GET['home_postcode']??''?>" placeholder="Home postcode">
+    <input type="text" name="student[password]" value="" placeholder="Password">
+    <input type="submit" value="Submit">
 </form>
